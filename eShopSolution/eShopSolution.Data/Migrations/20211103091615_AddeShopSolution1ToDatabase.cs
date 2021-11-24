f668c0ee607665a0503db206c181296956d17f44
tuan <tuanbosstrum@gmail.com>
2021-11-24 15:05:50 +0700

    sửa lại project net core entity framework core

---
+++ eShopSolution/eShopSolution.Data/Migrations/20211103091615_AddeShopSolution1ToDatabase.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class AddeShopSolution1ToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "08950345-a8df-49cd-bbb1-72ebfa6b850d");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "2d06b3ca-9925-40e6-9e01-deef82a5a238", "AQAAAAEAACcQAAAAEG6Xtyg7VEf35RLGmGVzZjYLn4ynz7JZQ451kGVgz2HAwByq9b8X8tlmdzQgHyM8Xg==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 11, 3, 16, 16, 15, 140, DateTimeKind.Local).AddTicks(2138));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "71cab679-dc76-4832-b7b5-6e4380d8e072");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "0d4caac6-979c-466e-a855-140aeb287920", "AQAAAAEAACcQAAAAEETQXAkncXoTzLXdGaxszgpqVn1XLlUHwwJL+qGD0UVdAMWai5ivwu5rjH+V4UHGPA==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 11, 3, 16, 15, 33, 87, DateTimeKind.Local).AddTicks(689));
+        }
+    }
+}
